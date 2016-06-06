8467ed3660d566874b03bd9408acbbecf71ae029
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-06-06 01:18:18 -0700

    started working on directive

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 var app = angular.module('mainApp', []);
+
+app.directive('makeEditable', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'makeEditable.html',
+    scope : true,
+    link : function(scope, element, attrs){
+    	scope.editable = false;
+    	scope.toggleEdit = function(){
+    		scope.editable = !scope.editable;
+    	};
+    }
+  };
+});
