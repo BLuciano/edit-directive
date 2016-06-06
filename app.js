86c2f66516833c72b70caddf5ecb6941cd098c4d
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-06-06 08:11:04 -0700

    Made directive an element and added transclusion

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var app = angular.module('mainApp', []);
 
 app.directive('makeEditable', function() {
   return {
-    restrict: 'E',
+    restrict: 'A',
     templateUrl: 'makeEditable.html',
+    transclude: true,
     scope : true,
     link : function(scope, element, attrs){
     	scope.editable = false;
